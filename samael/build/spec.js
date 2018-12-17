0503ed32ce611bba83e40e97a64218dc6f637ec2
Alexander Popov <aleksander.popov.93@gmail.com>
2018-12-17 23:34:04 +0200

    add webdriver-manager for samael demo

--- samael/build/spec.js
+++ samael/build/spec.js
@@ -4,20 +4,23 @@ const samael_1 = require("samael");
 const selenidejs_1 = require("selenidejs");
 const selenium_webdriver_1 = require("selenium-webdriver");
 samael_1.Suite('my suite', () => {
+    selenidejs_1.Browser.configuration.timeout = 10000;
+    selenidejs_1.Browser.configuration.screenshotPath = '';
     samael_1.BeforeEach(async () => {
         const webdriver = new selenium_webdriver_1.Builder()
-            .withCapabilities({ browserName: 'chrome' })
-            .usingServer('http://ip-5236.sunline.net.ua:4444/wd/hub')
+            .withCapabilities({ browserName: 'chrome', enableVNC: true })
+            .usingServer('http://localhost:4444/wd/hub')
             .build();
         selenidejs_1.Browser.configuration.webdriver = webdriver;
     });
     samael_1.AfterEach(async () => {
         await selenidejs_1.Browser.quit();
     });
-    Array(20).fill(null).forEach((_, index) => {
+    Array(10).fill(null).forEach((_, index) => {
         samael_1.Test(`my test #${index}`, async () => {
-            await selenidejs_1.Browser.open('http://the-internet.herokuapp.com/challenging_dom');
-            console.log(await selenidejs_1.Browser.title(), 'from test #', index);
+            await selenidejs_1.Browser.open('http://the-internet.herokuapp.com/dynamic_loading/2');
+            await selenidejs_1.Browser.element('button').click();
+            await selenidejs_1.Browser.element('#finish').should(selenidejs_1.have.text('Hello World!'));
         });
     });
 });
--- samael/spec.ts
+++ samael/spec.ts
@@ -1,13 +1,16 @@
-import { Suite, Test, BeforeEach, AfterEach } from 'samael';
-import { Browser } from 'selenidejs';
+import { AfterEach, BeforeEach, Suite, Test } from 'samael';
+import { Browser, have } from 'selenidejs';
 import { Builder } from 'selenium-webdriver';
 
 
 Suite('my suite', () => {
+    Browser.configuration.timeout = 10000;
+    Browser.configuration.screenshotPath = '';
+
     BeforeEach(async () => {
         const webdriver = new Builder()
-            .withCapabilities({browserName: 'chrome'})
-            .usingServer('...')
+            .withCapabilities({browserName: 'chrome', enableVNC: true})
+            .usingServer('http://localhost:4444/wd/hub')
             .build();
         Browser.configuration.webdriver = webdriver;
     });
@@ -16,11 +19,12 @@ Suite('my suite', () => {
         await Browser.quit();
     });
 
-    Array(20).fill(null).forEach((_, index) => {
+    Array(10).fill(null).forEach((_, index) => {
         Test(`my test #${index}`, async () => {
-            await Browser.open('http://the-internet.herokuapp.com/challenging_dom');
-            console.log(await Browser.title(), 'from test #', index);
+            await Browser.open('http://the-internet.herokuapp.com/dynamic_loading/2');
+            await Browser.element('button').click();
+            await Browser.element('#finish').should(have.text('Hello World!'));
         });
-    })
+    });
 
 });
